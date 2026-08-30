df9aa47801a591562f177ccf1d264403fc972e1f
malhotraanika01-sudo <malhotraanika01@gmail.com>
2026-08-31 04:18:59 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int,int> seen;
+        for(int i=0;i<nums.size();i++){
+            int need=target-nums[i];
+            if(seen.find(need)!=seen.end()){
+                return {seen[need],i};
+            }
+            seen[nums[i]]=i;
+        }
+    }
+};
